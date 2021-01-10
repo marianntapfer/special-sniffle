01fd6629b7e34195ff9ee8d88d28ab8907530a27
marianntapfer <tapferm@gmail.com>
2021-01-10 22:12:16 +0200

    opens a modal when you click on a Person to show his/hers extra details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import CardList from './CardList';
+import logo from './pipedrive.svg'
+import "./App.css";
 
 const App = () => {
+
+    const [show, setShow] = useState(false);
+
+    const openModalHandler = () => setShow(true)
+
+    const closeModalHandler = () => setShow(false)
+
     return (
         <div className="app">
-            <h1 className="mainTitle">People's List</h1>
-            <CardList />
+            <header className="appHeader">
+            <img src={logo} alt="" className="logo"/>
+            </header>
+            <div id="InfoCard" className={show ? 'show' : 'hide'}></div>
+            { show ? <div onClick={closeModalHandler} className="backgroundCover">
+            </div> : null}
+                <h1 className="mainTitle">People's List</h1>
+            <div className="peoplesList">
+                <CardList show={show} close={closeModalHandler} open={openModalHandler} />
+            </div>
         </div>
     );
 
