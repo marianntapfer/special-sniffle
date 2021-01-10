ff3905691a6b557e77c2db943969e5dc0326b143
marianntapfer <tapferm@gmail.com>
2021-01-10 16:16:36 +0200

    layouts look like brief

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { data } from "./data.json";
+import CardList from './CardList';
+import PersonsCard from './PersonsCard';
+import placeholder from './profilepic.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    let firstPerson = data[8]
+    return (
+        <div className="app">
+        	<PersonsCard
+        		picture={placeholder}
+        		name={firstPerson.name}
+        		phone={firstPerson.phone[0].value}
+        		email={firstPerson.email[0].value}
+        		organization={firstPerson.org_name}
+        		assistant={firstPerson['3d7fea2b018a23d5f846ba77b088b0b1936c7681']}
+        		groups={firstPerson['014ce6f24fd2bf978b81503a74699095ab5ddf1c']}
+        		location={firstPerson.org_id.address}
+        	/>
+            <h1 className="mainTitle">People's List</h1>
+            <CardList />
+        </div>
+    );
+
 }
-
 export default App;
