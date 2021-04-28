353a3e5dacf07a6c88515e936bf0ba78ca187c76
Mariann Tapfer <68736718+marianntapfer@users.noreply.github.com>
2021-04-28 13:26:50 +0300

    testing

--- anotherfile.js
+++
@@ -1,9 +0,0 @@
-console.log("heureka 2");
-console.log("another row");
-console.log("teine rida");
-console.log("another line of code that does nothing.");
-
-console.log("so much history.....");
-
-console.log("something else");
-console.log("this might work");
